2da5bd49c3cbe660d129cce63352329254adca8a
Robert Juncosa <bobbyjuncosa@gmail.com>
2017-11-13 17:52:31 -0500

    [release] 0.3.0

--- dist/PhoenixWebSocketLink.js
+++ dist/PhoenixWebSocketLink.js
@@ -13,27 +13,46 @@ var _apolloLinkCore = require('apollo-link-core');
 
 var _phoenix = require('phoenix');
 
-var _websocket = require('websocket');
-
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 var PhoenixWebSocketLink = exports.PhoenixWebSocketLink = function () {
+  /**
+   * @param {Object} opts 
+   * @param {string=/graphql} opts.uri - the URI key can be either a string endpoint or default to “/graphql”
+   * @param {Object=} opts.params - useful for passing an auth token, which could be parsed via Guardian in socket connect function
+   * @param {PhoenixSocket=PhoenixSocket} opts.socket - a pre-instantiated socket.  defaults to a new PhoenixSocket
+   * @param {W3CWebSocket=} opts.transport - useful for SSR (default PhoenixSocket transport calls window)
+   */
   function PhoenixWebSocketLink(opts) {
     _classCallCheck(this, PhoenixWebSocketLink);
 
-    if (typeof window === 'undefined' && !opts.transport) opts.transport = _websocket.w3cwebsocket;
-    var socket = new _phoenix.Socket(opts.uri, opts);
+    if (typeof window === 'undefined' && !opts.transport) {
+      console.warn('Potentially running in Node, which will cause errors (window undefined).  Consider setting w3cwebsocket as transport option.');
+    }
+    opts.uri = opts.uri || '/graphql';
+    this._socket = opts.socket || new _phoenix.Socket(opts.uri, opts);
 
     try {
-      socket.connect();
-      this._joinChannel(socket);
+      this._socket.connect();
+      this._joinChannel(this._socket);
     } catch (err) {
       console.error(err);
     }
   }
 
-  // Required by the NetworkInterface interface spec
-  // param: { variables, extensions, operationName, query }
+  /**
+   * A link’s request is called every time execute is run on that link chain (typically every operation). 
+   * The request is where the operation is given to the link to return back data of some kind.
+   * @param {Object} operation
+   * @param {DocumentNode} operation.query - A DocumentNode (parsed GraphQL Operation) describing the operation taking place
+   * @param {Object} operation.variables - A map of variables being sent with the operation
+   * @param {string} operation.operationName - A string name of the query if it is named, otherwise it is null
+   * @param {Object} operation.extensions - A map to store extensions data to be sent to the server
+   * @param {function} operation.getContext - A function to return the context of the request
+   * @param {function} operation.setContext - A function that takes either a new context object, or a function which receives the previous context and retuns a new one. (Think of it like setState from React)
+   * @param {function} operation.toKey - A function to convert the current operation into a string to be used as a unique identifier
+   * @returns {Observable}
+   */
 
 
   _createClass(PhoenixWebSocketLink, [{
@@ -46,7 +65,6 @@ var PhoenixWebSocketLink = exports.PhoenixWebSocketLink = function () {
           variables = _ref.variables;
 
       query = (0, _printer.print)(query);
-      // console.log('Phoenix apollo: operationName:', operationName)
       return new _apolloLinkCore.Observable(function (observer) {
         _this._channel.push('doc', { operationName: operationName, query: query, variables: variables }).receive('ok', function (response) {
           // console.log('operation response', response)
@@ -58,6 +76,11 @@ var PhoenixWebSocketLink = exports.PhoenixWebSocketLink = function () {
         }).receive('error', observer.error.bind(observer)).receive('timeout', observer.error.bind(observer));
       });
     }
+
+    /**
+     * @private
+     */
+
   }, {
     key: '_joinChannel',
     value: function _joinChannel(socket) {
