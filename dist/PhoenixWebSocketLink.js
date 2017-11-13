20b8b689cf6304cf95ff7ea6577bfbeb45397ed0
Robert Juncosa <bobbyjuncosa@gmail.com>
2017-11-13 18:02:11 -0500

    [release] v0.3.2

--- dist/PhoenixWebSocketLink.js
+++ dist/PhoenixWebSocketLink.js
@@ -18,7 +18,7 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
 var PhoenixWebSocketLink = exports.PhoenixWebSocketLink = function () {
   /**
    * @param {Object} opts 
-   * @param {string=/graphql} opts.uri - the URI key can be either a string endpoint or default to “/graphql”
+   * @param {string=/socket} opts.uri - the URI key can be either a string endpoint or default to “/socket
    * @param {Object=} opts.params - useful for passing an auth token, which could be parsed via Guardian in socket connect function
    * @param {PhoenixSocket=PhoenixSocket} opts.socket - a pre-instantiated socket.  defaults to a new PhoenixSocket
    * @param {W3CWebSocket=} opts.transport - useful for SSR (default PhoenixSocket transport calls window)
@@ -29,7 +29,7 @@ var PhoenixWebSocketLink = exports.PhoenixWebSocketLink = function () {
     if (typeof window === 'undefined' && !opts.transport) {
       console.warn('Potentially running in Node, which will cause errors (window undefined).  Consider setting w3cwebsocket as transport option.');
     }
-    opts.uri = opts.uri || '/graphql';
+    opts.uri = opts.uri || '/socket';
     this._socket = opts.socket || new _phoenix.Socket(opts.uri, opts);
 
     try {
