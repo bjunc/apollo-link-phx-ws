f1667a33239db8cefa5254ba73d098d5d561e694
Robert Juncosa <bobbyjuncosa@gmail.com>
2017-11-13 17:50:21 -0500

    added docs

--- src/PhoenixWebSocketLink.js
+++ src/PhoenixWebSocketLink.js
@@ -4,6 +4,13 @@ import { Socket as PhoenixSocket } from 'phoenix'
 import { w3cwebsocket as W3CWebSocket } from 'websocket'
 
 export class PhoenixWebSocketLink {
+  /**
+   * @param {Object} opts 
+   * @param {string=/graphql} opts.uri - the URI key can be either a string endpoint or default to “/graphql”
+   * @param {Object=} opts.params - useful for passing an auth token, which could be parsed via Guardian in socket connect function
+   * @param {PhoenixSocket=PhoenixSocket} opts.socket - a pre-instantiated socket.  defaults to a new PhoenixSocket
+   * @param {W3CWebSocket=} opts.transport - useful for SSR (default PhoenixSocket transport calls window)
+   */
   constructor (opts) {
     if (typeof window === 'undefined' && !opts.transport) opts.transport = W3CWebSocket
     let socket = new PhoenixSocket(opts.uri, opts)
@@ -16,8 +23,19 @@ export class PhoenixWebSocketLink {
     }
   }
 
-  // Required by the NetworkInterface interface spec
-  // param: { variables, extensions, operationName, query }
+  /**
+   * A link’s request is called every time execute is run on that link chain (typically every operation). 
+   * The request is where the operation is given to the link to return back data of some kind.
+   * @param {Object} operation
+   * @param {DocumentNode} operation.query - A DocumentNode (parsed GraphQL Operation) describing the operation taking place
+   * @param {Object} operation.variables - A map of variables being sent with the operation
+   * @param {string} operation.operationName - A string name of the query if it is named, otherwise it is null
+   * @param {Object} operation.extensions - A map to store extensions data to be sent to the server
+   * @param {function} operation.getContext - A function to return the context of the request
+   * @param {function} operation.setContext - A function that takes either a new context object, or a function which receives the previous context and retuns a new one. (Think of it like setState from React)
+   * @param {function} operation.toKey - A function to convert the current operation into a string to be used as a unique identifier
+   * @returns {Observable}
+   */
   request ({ operationName, query, variables }) {
     query = print(query)
 		// console.log('Phoenix apollo: operationName:', operationName)
@@ -37,6 +55,10 @@ export class PhoenixWebSocketLink {
 				.receive('timeout', observer.error.bind(observer))
 		})
   }
+
+  /**
+   * @private
+   */
 
   _joinChannel (socket) {
 		const CHANNEL_TOPIC = '__absinthe__:control'
